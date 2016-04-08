41390ebc434dee361b130e8f51fb2b32abeefbe4
leemason <contact@nohalfpixels.com>
2016-04-08 23:27:51 +0100

    a little more dev

--- public/index.php
+++ public/index.php
@@ -2,11 +2,20 @@
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
+$router = $app->get(\Ecfectus\Router\Router::class);
+
+$router->get('/public[/{name}]', 'test');
+
 $kernel = $app->get(\Ecfectus\Framework\Http\Kernel::class);
+
+/*
+$kernel->pushMiddleware(function($r, $resp, $next){
+   return $next($r, $resp->withStatus(200));
+});
+*/
 
 $request = $app->get(\Psr\Http\Message\ServerRequestInterface::class);
 
 $response = $app->get(\Psr\Http\Message\ResponseInterface::class);
-
 
 $kernel->handle($request, $response);
